0c04cf6cec73e630638d1d3af5cb3414adb745e7
alagrin <grinny@icloud.com>
2020-04-27 11:08:53 -0700

    added main code for next time to get output data in proper format

--- iex_sheet.py
+++ iex_sheet.py
@@ -35,3 +35,33 @@ endpoints = 'price,stats'
 HTTP_request = f'https://cloud.iexapis.com/stable/stock/market/batch?symbols={ticker_string}&types={endpoints}&range=1y&token={IEX_API_key}'
 
 raw_data = pd.read_json(HTTP_request)
+
+# Create an empty pandas DataFrame to append our parsed values into during our for loop
+output_data = pd.DataFrame(pd.np.empty((0, 4)))
+
+for ticker in raw_data.columns:
+
+    # Parse the company's name
+    company_name = raw_data[ticker]['stats']['companyName']
+
+    # Parse the company's stock price
+    stock_price = raw_data[ticker]['price']
+
+    # Parse the company's dividend yield
+    dividend_yield = raw_data[ticker]['stats']['dividendYield']
+
+    new_column = pd.Series([ticker, company_name, stock_price, dividend_yield])
+    output_data = output_data.append(new_column, ignore_index=True)
+
+# Change the column names of output_data
+output_data.columns = ['Ticker', 'Company Name',
+                       'Stock Price', 'Dividend Yield']
+
+# Change the index of output_data
+output_data.set_index('Ticker', inplace=True)
+
+# Replace the missing values of the 'Dividend Yield' column with 0
+output_data['Dividend Yield'].fillna(0, inplace=True)
+
+# Print the DataFrame
+output_data
